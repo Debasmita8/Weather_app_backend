4cf83870b9464be024270918d86fa1d7ab335e2e
Debasmita8 <140266871+Debasmita8@users.noreply.github.com>
2024-07-13 19:34:15 +0530

    Add files via upload

---
+++ Weathe_app.py
@@ -0,0 +1,22 @@
+import requests
+
+api_key ='a6a8b2894aa0924aa4150dc42ba3f326'
+
+user_input = input("Enter the city: ")
+
+response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+#if print(response.status_code) outputs to 200, the it means the request was succesful.
+
+if (response.json()['cod'] == '404'):
+#'cod' is the status code that the api returns.
+    print("City does not exist")
+else:
+    Weather = response.json()['weather'][0]['main']
+    Temp = round(((response.json()['main']['temp'])-30)/2)
+    feel_temp = round(((response.json()['main'][0]['feels_like'])-30)/2)
+    humidity = response.json()['main'][4]['humidity']
+
+    print(f"Weather in {user_input}: {Weather}")
+    print(f"Temperature in {user_input}: {Temp}°C but it feels like: {feel_temp}°C")
+    print(f"Humidity in {user_input}: {humidity}%")
+    #To get the degree symbol alt+0176.
